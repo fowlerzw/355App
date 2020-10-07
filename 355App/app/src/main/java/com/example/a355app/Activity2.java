49e2588ba359f8c8b81721eebb5049aa4b30c744
Zachary Fowler <fowlerzw@mymail.vcu.edu>
2020-10-07 09:58:19 -0400

    Created 4th activity and connected through buttons, also labeled and fixed some of the comments throughout

--- 355App/app/src/main/java/com/example/a355app/Activity2.java
+++ 355App/app/src/main/java/com/example/a355app/Activity2.java
@@ -14,16 +14,22 @@ public class Activity2 extends AppCompatActivity {
         setContentView(R.layout.activity_2);
     }
 
-    //this method opens up the MainActivity scene which is the filters page
+    //this method opens up the MainActivity scene which is the main page
     public void openMainActivity(View view){
         Intent Mainintent = new Intent(this, MainActivity.class);
         startActivity(Mainintent);
     }
 
-    //this method opens up the Activity3 scene which is the filters page
+    //this method opens up the Activity3 scene which is the tips page
     public void openActivity3(View view){
         Intent act3intent = new Intent(this, Activity3.class);
         startActivity(act3intent);
+    }
+
+    //this method opens up the Activity4 scene which is the roll page
+    public void openActivity4(View view){
+        Intent act4intent = new Intent(this, Activity4.class);
+        startActivity(act4intent);
     }
 
 
---
+++ 355App/app/src/main/java/com/example/a355app/Activity4.java
@@ -0,0 +1,34 @@
+package com.example.a355app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Activity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_4);
+    }
+
+    //this method opens up the Activity Main scene which is the main page
+    public void openActivityMain(View view){
+        Intent actMainintent = new Intent(this, MainActivity.class);
+        startActivity(actMainintent);
+    }
+
+    //this method opens up the Activity2 scene which is the filters page
+    public void openActivity2(View view){
+        Intent act2intent = new Intent(this, Activity2.class);
+        startActivity(act2intent);
+    }
+
+    //this method opens up the Activity3 scene which is the tips page
+    public void openActivity3(View view){
+        Intent act3intent = new Intent(this, Activity3.class);
+        startActivity(act3intent);
+    }
+}
